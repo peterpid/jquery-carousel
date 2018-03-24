0e1520a0de5dca168165cb406e428028bc1600d9
Piotr Pidlypczak <peterpid@gmail.com>
2018-03-24 10:18:28 +0100

    Fix carousel animation, improve layout

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
+var imageHeight = 500;
+var imageWidth  = 800;
+
 $(function(){
-	var carouselList = $("#carousel ul");
+	var carouselList = $('#carousel ul');
 	console.log('before setTimeout');
 	setTimeout(changeSlide, 3000); //after 3 seconds will change the slide once
 	setInterval(changeSlide, 3000); //every 3 seconds, it performs a function to change the slide
 	
 	function changeSlide() {
 		console.log('changeSlide');
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+		carouselList.animate({'marginLeft':-1*imageWidth}, 500, moveFirstSlide);
 	}
 
 	function moveFirstSlide() {
 		console.log('moveFirstSlide');
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
-
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
 	}
