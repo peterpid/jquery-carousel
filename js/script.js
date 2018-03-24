db79ad5cbf9d99d63d4b2c3bafe064cc317645c7
Piotr Pidlypczak <peterpid@gmail.com>
2018-03-24 11:45:58 +0100

    Remove additional slide animation at page load, remove logs

--- js/script.js
+++ js/script.js
@@ -3,17 +3,13 @@ var imageWidth  = 800;
 
 $(function(){
 	var carouselList = $('#carousel ul');
-	console.log('before setTimeout');
-	setTimeout(changeSlide, 3000); //after 3 seconds will change the slide once
 	setInterval(changeSlide, 3000); //every 3 seconds, it performs a function to change the slide
 	
 	function changeSlide() {
-		console.log('changeSlide');
 		carouselList.animate({'marginLeft':-1*imageWidth}, 500, moveFirstSlide);
 	}
 
 	function moveFirstSlide() {
-		console.log('moveFirstSlide');
 		var firstItem = carouselList.find('li:first');
 		var lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
