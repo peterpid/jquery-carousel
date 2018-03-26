dc65c3964df66883bad0c9612d327d9cebc36b83
Piotr Pidlypczak <peterpid@gmail.com>
2018-03-26 20:11:32 +0200

    Add left, right button handling part1
    TODO: slide right animation to be corrected

--- js/script.js
+++ js/script.js
@@ -1,18 +1,48 @@
-var imageHeight = 500;
-var imageWidth  = 800;
+
 
 $(function(){
+
+	var imageHeight = 500;
+	var imageWidth  = 800;
+	var animationIntervalMs = 3000;
 	var carouselList = $('#carousel ul');
-	setInterval(changeSlide, 3000); //every 3 seconds, it performs a function to change the slide
-	
+	var direction = 'left';
+	var intervalId = setInterval(changeSlide, animationIntervalMs);
+
 	function changeSlide() {
-		carouselList.animate({'marginLeft':-1*imageWidth}, 500, moveFirstSlide);
+		if (direction === 'left') {
+			carouselList.animate({'marginLeft':-1*imageWidth}, 500, moveFirstSlide);
+		} else if (direction === 'right') {
+			carouselList.animate({'marginLeft':imageWidth}, 500, moveFirstSlide);
+		}
 	}
 
 	function moveFirstSlide() {
 		var firstItem = carouselList.find('li:first');
 		var lastItem = carouselList.find('li:last');
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
+		console.log('moveFirstSlide: ' + direction);
+
+		if (direction === 'left') {
+			lastItem.after(firstItem);
+		} else {
+			lastItem.insertBefore(firstItem);
+		}
+		carouselList.css({'marginLeft':0});
 	}
+
+	var buttonLeft = $('#btn-left');
+	buttonLeft.click(function(){
+		direction = 'left';
+		clearInterval(intervalId);
+		changeSlide();
+		intervalId = setInterval(changeSlide, animationIntervalMs);
+	});
+
+	var buttonRight = $('#btn-right');
+	buttonRight.click(function(){
+		direction = 'right';
+		clearInterval(intervalId);
+		changeSlide();
+		intervalId = setInterval(changeSlide, animationIntervalMs);
+	});
 });
